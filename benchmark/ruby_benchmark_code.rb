ce50f96985dec627ddc424938c694db9d1646546
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-10-15 22:28:02 +0900

    Changed benchmark code.

--- benchmark/ruby_benchmark_code.rb
+++ benchmark/ruby_benchmark_code.rb
@@ -13,6 +13,40 @@ class RubyBenchmarkCode
     end
   end
 
+  def tak2( x, y, z )
+    @inner_tak = Proc.new { |_x,_y,_z| 
+      if ( _y >= _x )
+        _y
+      else
+        @inner_tak.call(
+                        @inner_tak.call( _x-1, _y, _z ),
+                        @inner_tak.call( _y-1, _z, _x ),
+                        @inner_tak.call( _z-1, _x, _y ))
+      end
+    }
+    @inner_tak.call( x, y, z )
+  end
+
+  def tak3( x, y, z )
+    @inner_minus = Proc.new { |_a,_b|
+      _a - _b
+    }
+    @inner_le  = Proc.new { |_a,_b|
+      _a >= _b
+    }
+    @inner_tak = Proc.new { |_x,_y,_z| 
+      if ( @inner_le.call( _y, _x ))
+        _y
+      else
+        @inner_tak.call(
+                        @inner_tak.call( @inner_minus.call(_x,1), _y, _z ),
+                        @inner_tak.call( @inner_minus.call(_y,1), _z, _x ),
+                        @inner_tak.call( @inner_minus.call(_z,1), _x, _y ))
+      end
+    }
+    @inner_tak.call( x, y, z )
+  end
+
   def fact( n )
     if 0 == n
       1
