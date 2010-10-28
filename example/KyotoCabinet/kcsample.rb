20785d4d1abe7b8fbbc8637a206ec21a804ed43e
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-10-28 10:55:48 +0900

    Added KyotoCabinet's sample code.

---
+++ example/KyotoCabinet/kcsample.rb
@@ -0,0 +1,22 @@
+#!/usr/local/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'kyotocabinet'
+
+db = KyotoCabinet::DB.new
+# データベースを開く
+unless db.open('sample.kch', KyotoCabinet::DB::OWRITER | KyotoCabinet::DB::OCREATE)
+  printf("open error: %s\n", db.error)
+end
+
+# 書きこみ
+db.set( 'key1', 'value1' )
+db.set( 'key2', 'value2' )
+db.set( 'key3', 'value3' )
+
+# 内容確認
+db.each { |key, value|
+  printf( "key=[%s] value=[%s]\n", key,value )
+}
+
+db.close
