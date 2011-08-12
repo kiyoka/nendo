2968657344219346d451f1b21992a4ec67d1d3bd
Kiyoka Nishiyama <kiyoka@sumibi.org>
2011-08-12 20:01:59 +0900

    Added rspec formatter for Emacs M-x compile mode.

--- test/rspec_formatter_for_emacs.rb
+++ test/rspec_formatter_for_emacs.rb
@@ -1,11 +1,25 @@
-require 'spec/runner/formatter/progress_bar_formatter'
+require 'rspec/core/formatters/progress_formatter'
 
 # Example of a formatter with custom bactrace printing. Run me with:
-# ruby bin/spec examples/failing -r examples/passing/custom_formatter.rb -f CustomFormatter
-class CustomFormatter < Spec::Runner::Formatter::ProgressBarFormatter
+# ruby bin/spec xxxxx.rb -r ./test/rspec_formatter_for_emacs.rb -f CustomFormatter
+class CustomFormatter < RSpec::Core::Formatters::ProgressFormatter
   def backtrace_line(line)
-    line.gsub(/([^:]*\.rb):(\d*)/) do
-      "<a href=\"file://#{File.expand_path($1)}\">#{$1}:#{$2}</a> "
+    str = line.gsub(/([^:]*\.rb):([0-9]+):in /) do
+      path   = "#{$1}"
+      lineno = "#{$2}"
+      if path.match( /lib/ ) and path.match( /rspec/ )
+        "#{File.expand_path(path)}:#{lineno} IN "
+      else
+        "#{File.expand_path(path)}:#{lineno}:in "
+      end
+    end
+    str
+  end
+
+  def dump_backtrace(example)
+    format_backtrace(example.execution_result[:exception].backtrace, example).each do |backtrace_info|
+      output.puts cyan("    #{backtrace_info}")
     end
   end
+
 end
