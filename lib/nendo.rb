522a1cec840bea23aacb1d79a66f1657515463d2
Kiyoka Nishiyama <kiyoka@sumibi.org>
2011-10-26 06:02:13 +0900

    Moved Core classe to lib/core.rb

--- lib/nendo.rb
+++ lib/nendo.rb
@@ -33,122 +33,15 @@
 #   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+require 'stringio'
+require 'digest/sha1'
+require 'pp'
 require 'ruby/types'
 require 'ruby/reader'
 require 'ruby/builtin_functions'
 require 'ruby/evaluator'
 require 'ruby/printer'
-
-module Nendo
-  require 'stringio'
-  require 'digest/sha1'
-  require 'pp'
-
-  class Core
-    def initialize( debug_evaluator = false, debug_printer = false )
-      @debug_evaluator = debug_evaluator
-      @evaluator       = Evaluator.new( self, debug_evaluator )
-      @debug_printer   = debug_printer
-    end
-
-    def self.version
-      "0.5.4"  ##NENDO-VERSION
-    end
-
-    attr_reader :evaluator
-
-    def loadInitFile( use_compiled = true )
-      done = false
-      if use_compiled 
-        compiled_file = File.dirname(__FILE__) + "/init.nndc"
-        if File.exist?( compiled_file )
-          @evaluator.__PAMARKload_MIMARKcompiled_MIMARKcode( compiled_file )
-          done = true
-        end
-      end
-      unless done
-        @evaluator.__PAMARKload( File.dirname(__FILE__) + "/init.nnd" )
-        @evaluator.__PAMARKload( File.dirname(__FILE__) + "/init.nnd" ) # for %tailcall compile for init.nnd
-      end
-    end
-  
-    def load( path )
-      @evaluator.__PAMARKload( path )
-    end
-  
-    def load_compiled_code( path )
-      @evaluator.__PAMARKload_MIMARKcompiled_MIMARKcode( path )
-    end
-  
-    def load_compiled_code_from_string( rubyExp )
-      @evaluator._load_MIMARKcompiled_MIMARKcode_MIMARKfrom_MIMARKstring( rubyExp )
-    end
-  
-    def setArgv( argv )
-      @evaluator.setArgv( argv )
-    end
-
-    def setOptimizeLevel( level )
-      @evaluator.setOptimizeLevel( level )
-    end
-
-    def setDisplayErrors( flag )
-      @evaluator.setDisplayErrors( flag )
-    end
-
-    def clean_compiled_code
-      @evaluator._clean_MIMARKcompiled_MIMARKcode()
-    end
-  
-    def prompt
-      STDERR.print "nendo> "
-    end
-  
-    def repl
-      printer = Printer.new( @debug_printer )
-      reader  = Reader.new( STDIN, "(stdin)", false )
-      self.prompt
-      while true
-        begin
-          lineno = reader.lineno
-          s = reader._read
-          if s[1] # EOF?
-            break
-          elsif not s[0].is_a? Nil
-            printf( "\n          readExp=<<< %s >>>\n", printer._write(s[0]) ) if @debug_evaluator
-            STDERR.print printer._write( @evaluator.lispEval( s[0], reader.sourcefile, lineno )) + "\n"
-            self.prompt
-          end
-        rescue => e
-          print e.message + "\n"
-          e.backtrace.each { |x| printf( "\tfrom %s\n", x ) }
-          print "\n"
-          self.prompt
-        end
-      end
-    end
-  
-    def evalStr( str )
-      printer   = Printer.new( @debug_printer )
-      sio       = StringIO.open( str )
-      reader    = Reader.new( sio, "(string)", false )
-      result    = nil
-      while true
-        lineno = reader.lineno
-        s = reader._read
-        if s[1] # EOF?
-          break
-        elsif not s[0].is_a? Nil
-          printf( "\n          readExp=<<< %s >>>\n", printer._write(s[0]) ) if @debug_evaluator
-          result = printer._write( @evaluator.lispEval( s[0], reader.sourcefile, lineno )) 
-        end
-      end
-      result
-    end
-  end
-end
-
-
+require 'ruby/core'
 
 
 class Symbol
